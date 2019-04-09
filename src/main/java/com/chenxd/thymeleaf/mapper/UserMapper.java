a75850440fe41f4caea6b3a531e91fad5be12691
chenxd1991 <18246068557@163.com>
2019-04-09 23:39:40 +0800

    springboot+druid

--- src/main/java/com/chenxd/thymeleaf/mapper/UserMapper.java
+++ src/main/java/com/chenxd/thymeleaf/mapper/UserMapper.java
@@ -11,8 +11,20 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
     public User getUser(String id);
+
+    /**
+     * 使用Select注解，同时还有Insert/Update/Delete
+     * @return
+     */
     @Select("select name,age,phone from user")
     public List<User> getUsers();
+
+    /**
+     * 使用SelectProvider 进行sql拼写，同时还有InsertProvider/UpdateProvider/DeleteProvider
+     * @param id
+     * @return
+     */
     @SelectProvider(type = SqlProvider.class,method = "getUser")
     public User getUserInfo(String id);
+
 }
